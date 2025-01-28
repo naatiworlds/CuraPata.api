9dd6b370b6b71848bd84916a0836518d8747a593
naatii <jcorber855@gmail.com>
2025-01-28 20:15:43 +0100

    Manejar actualizaciones de endopoints sin fotos o con foto opcional

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -14,12 +14,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Esto simula _
 const upload = (tipoArchivo) => {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
+      if (!file) return cb(null, false); // Si no hay archivo, continuar sin error
       let uploadPath = "";
 
-      // Aquí usamos el tipo de archivo como parámetro para determinar la carpeta
       switch (tipoArchivo) {
         case "perfil":
-          uploadPath = path.join(__dirname, "..", "uploads", "usuarios"); // Asegúrate de que la ruta es correcta
+          uploadPath = path.join(__dirname, "..", "uploads", "usuarios");
           break;
         case "producto":
           uploadPath = path.join(__dirname, "..", "uploads", "productos");
@@ -34,22 +34,18 @@ const upload = (tipoArchivo) => {
           return cb(new Error("Tipo de archivo no válido"));
       }
 
-
-      // Crear la carpeta si no existe
       if (!fs.existsSync(uploadPath)) {
         fs.mkdirSync(uploadPath, { recursive: true });
       }
 
-      cb(null, uploadPath); // Ruta donde se guardará el archivo
+      cb(null, uploadPath);
     },
     filename: async (req, file, cb) => {
+      if (!file) return cb(null, false); // Continuar si no hay archivo
       try {
-        const id = req.params.id; // Usamos el id pasado en los parámetros de la URL
-
+        const id = req.params.id;
         let modelo;
-        let documento;
 
-        // Dependiendo del tipo de archivo, buscamos el modelo correspondiente
         switch (tipoArchivo) {
           case "perfil":
             modelo = Usuarios;
@@ -67,25 +63,21 @@ const upload = (tipoArchivo) => {
             return cb(new Error("Tipo de archivo no válido"));
         }
 
-        // Buscamos el documento correspondiente por ID
-        documento = await modelo.findById(id);
+        const documento = await modelo.findById(id);
         if (!documento) {
-          return cb(new Error(`${tipoArchivo.charAt(0).toUpperCase() + tipoArchivo.slice(1)} no encontrado`));
+          return cb(new Error(`${tipoArchivo} no encontrado`));
         }
 
-
-        // Obtenemos la extensión del archivo original
         const fileExtension = path.extname(file.originalname);
-
-        // Creamos el nombre del archivo con la ID del documento y la extensión
         cb(null, `${documento._id}${fileExtension}`);
       } catch (error) {
-        cb(error); // En caso de error, lo pasamos a la función de callback
+        cb(error);
       }
     },
   });
 
   return multer({ storage: storage });
 };
+
 
 export default upload;
